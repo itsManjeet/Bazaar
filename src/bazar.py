909cf33bd141f19b95c9e8be6352b633597e5e03
itsmanjeet <itsmanjeet1998@gmail.com>
2020-02-27 03:21:07 +0530

    enable searching of application with name, summary, description

--- src/bazar.py
+++ src/bazar.py
@@ -26,11 +26,13 @@ class BazarBack:
                 'summary': cmpnt.find('summary').text,
                 'description': self.__getDesc(cmpnt),
                 'icons': self.__getIcons(cmpnt),
-                'categories': self.__getcategories(cmpnt)
+                'categories': self.__getcategories(cmpnt),
+                'url': self.__getURL(cmpnt)
             }
 
             self.AppData.append(appData)
 
+        print('Total Apps',len(self.AppData))
         return self.AppData
     def __getDesc(self,cmpnt):
         try:
@@ -68,16 +70,29 @@ class BazarBack:
         except AttributeError:
             return None
           
+    def __getURL(self, cmpnt):
+        allUrls = cmpnt.findall('url')
+        urls = []
+        for u in allUrls:
+            url = {
+                'type': u.attrib['type'],
+                'address': u.text
+            }
 
+            urls.append(url)
+
+        return urls
 
 class Bazar:
     def __init__(self):
         self.bg = BazarBack('/var/lib/appstream-extractor/export-data/appstream-flathub-x86_64-2020-02-26.04:28:37.+0000/appstream.xml')
-        for app in self.bg.GetAppData():
+
+    def UpdateAppsList(self,data):
+        for app in data:
             try:
                 img = GdkPixbuf.Pixbuf.new_from_file(IconsData + '/64x64/' + app['icons'][0]['file'])
             except:
-                img = Gtk.IconTheme.get_default().load_icon('edit-cut', 64, 0)
+                img = Gtk.IconTheme.get_default().load_icon('gtk-missing-image', 64, 0)
             listStore.append([img,app['name']])
 
 
@@ -97,6 +112,22 @@ class Handler:
                 DescriptionLabel.set_text(i['description'])
                 AppImage.set_from_pixbuf(SelectedAppIcon)
                 InstallerButton.connect('clicked', self.InstallApp, i)
+
+    def SearchChanged(self, searchWidget):
+        listStore.clear()
+        searchText = searchWidget.get_text()
+        searchText = searchText.lower()
+        finalData = b.bg.AppData
+        if len(searchText) != 0:
+            d = []
+            for x in finalData:
+                if searchText in x['name'].lower() or searchText in x['summary'].lower() or searchText in x['description'].lower():
+                    d.append(x)
+            finalData = d
+
+        b.UpdateAppsList(finalData)
+
+
 
     def InstallApp(self, widget, Data):
         print(widget, Data)
@@ -125,6 +156,8 @@ IconListView.set_text_column(1)
 Builder.connect_signals(Handler())
 
 b = Bazar()
+b.bg.GetAppData()
+b.UpdateAppsList(b.bg.AppData)
 
 Window.show_all()
 Gtk.main()
